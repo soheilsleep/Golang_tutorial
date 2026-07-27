87dbb3d5c5570b4d9c83a416eccab527f273caee
soheil kazemi <soheilsleep788@gmail.com>
2026-07-27 16:42:41 +0330

    adding Variables and constants . then explaining the ways we can use Variables

---
+++ 04-VariablesAndConstants/Variable.go
@@ -0,0 +1,31 @@
+package main
+//method  1
+
+var i int = 12
+var s string = "first name"
+
+//method 2
+var i2 int
+var s2 string
+
+//method 3
+var i3 = 22
+var f1 = 12.66
+var s3 = "hello world"
+
+//method 4
+i4 := 12
+f2 := 12.21
+s4 := "Hello World"
+
+//method 5 group variables
+var i5, f3, s5 = 25, 66.66, "Hello again"
+ i5, f3, s5 = 25, 66.66, "Hello again with out (var)"
+
+ // method 6 group variables in another way
+  var (
+	i6 = 45
+	f4 = 45.35
+	s6 = "the end"
+  )
+

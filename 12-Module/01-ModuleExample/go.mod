ee6377e140039b949a8c44469856b930481079d5
soheil kazemi <soheilsleep788@gmail.com>
2026-08-05 16:43:41 +0330

    learned how to import local packages and how to replace foreign packages to a local one

--- 12-Module/01-ModuleExample/go.mod
+++ 12-Module/01-ModuleExample/go.mod
@@ -3,3 +3,5 @@ module github.com/soheilsleep/ModuleExample
 go 1.26.5
 
 require github.com/jalaali/go-jalaali v0.0.0-20250521085720-bf793ab67800 // indirect
+replace github.com/jalaali/go-jalaali => ./locall/go-jalaali
+
--- 12-Module/01-ModuleExample/main.go
+++ 12-Module/01-ModuleExample/main.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	jalaali "github.com/jalaali/go-jalaali"
+	"github.com/soheilsleep/ModuleExample/services"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	year, month, day, error := jalaali.ToGregorian(1399, 1, 1)
+	year, month, day, error := jalaali.ToGregorian(1399, 13, 12)
 	if error == nil {
 		fmt.Printf("Year: %d, Month: %d, Day: %d\n", year, month, day)
 	} else {
 		fmt.Printf("Error: %v\n", error)
 	}
+	var service services.TestService = services.TestService{}
+	fmt.Printf("%v\n", service)
 }
